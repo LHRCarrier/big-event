95e2ce80ed87db3a3446f53840e7f537a0d3a9dc
羁木轮 <1844622298@qq.com>
2026-04-16 17:16:21 +0800

    Merge remote-tracking branch 'big-event/master'
    Signed-off-by: 羁木轮 <1844622298@qq.com>

--- src/main/java/com/bubbles/server/controller/user/ArticleController.java
+++ src/main/java/com/bubbles/server/controller/user/ArticleController.java
@@ -1,15 +1,26 @@
 package com.bubbles.server.controller.user;
 
 import com.bubbles.common.Result;
+import com.bubbles.pojo.vo.UserVO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
+    /**
+     * 查询文章
+     * @return
+     */
     @GetMapping("/list")
     public Result<String> list(){
+        return Result.success("<UNK>");
+    }
+
+    @PutMapping("/update")
+    public Result<String> update(){
         return Result.success("<UNK>");
     }
 }
